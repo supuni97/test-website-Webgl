65a8b01e183b31f5b144fe7e1c3f985036fa4fc3
supuni <sitharabandara1997@gmail.com>
2024-06-09 11:27:16 +0530

    initial commit

---
+++ src/UnityGame.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const UnityGame = () => {
+  return (
+    <div>
+      <h1>My Unity WebGL Game</h1>
+      <iframe
+        src="https://heroic-dusk-6d491e.netlify.app/"
+        width="100%"
+        height="600px"
+        title="Unity WebGL Game"
+      />
+    </div>
+  );
+};
+
+export default UnityGame;
